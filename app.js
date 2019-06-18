83fbd363a3b47bfaabac5998773c5071dccf45a7
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-18 14:47:09 -0700

    results are up

--- app.js
+++ app.js
@@ -1,125 +1,106 @@
 'use strict';
 
-/*
-
-Display a list of the products with votes received with each list item looking like "3 votes for the Banana Slicer".
-
-*/
-
 // Globals
-var imageSection = document.getElementById('images');
+var imageSection = document.getElementById('allImages');
 var leftImage = document.getElementById('left_image');
 var middleImage = document.getElementById('middle_image');
 var rightImage = document.getElementById('right_image');
 
-var totalClicks = 0;
+var clickCount = 0;
+var maxClicks = 25;
 
-var leftProductOnPage = null;
-var middleProductOnPage = null;
-var rightProductOnPage = null;
-
-var ProductImage = function(name, imgSrc, id){
+var ProductImage = function(name, imgSrc){
   this.name = name;
   this.url = imgSrc;
-  this.timesShown = 0;
   this.clicks = 0;
-  this.id = id;
-
+  this.timesShown = 0;
   ProductImage.allImages.push(this);
-}
+};
 
 ProductImage.allImages = [];
+ProductImage.previousImages = [];
 
 // Instantiate all image objects
-new ProductImage('bag', './img/bag.jpg');
-new ProductImage('banana', './img/banana.jpg');
-new ProductImage('bathroom', './img/bathroom.jpg');
-new ProductImage('boots', './img/boots.jpg');
-new ProductImage('breakfast', './img/breakfast.jpg');
-new ProductImage('bubblegum', './img/bubblegum.jpg');
-new ProductImage('chair', './img/chair.jpg');
-new ProductImage('cthulhu', './img/cthulhu.jpg');
-new ProductImage('dog-duck', './img/dog-duck.jpg');
-new ProductImage('dragon', './img/dragon.jpg');
-new ProductImage('pen', './img/pen.jpg');
-new ProductImage('pet-sweep', './img/pet-sweep.jpg');
-new ProductImage('scissors', './img/scissors.jpg');
-new ProductImage('shark', './img/shark.jpg');
-new ProductImage('sweep', './img/sweep.png');
-new ProductImage('tauntaun', './img/tauntaun.jpg');
-new ProductImage('unicorn', './img/unicorn.jpg');
-new ProductImage('usb', './img/usb.gif');
-new ProductImage('water-can', './img/water-can.jpg');
-new ProductImage('wine-glass', './img/wine-glass.jpg');
+var buildProducts = function(){
 
-
-var leftProductOnPage = null;
-var middleProductOnPage = null;
-var rightProductOnPage = null;
-
-
-var renderProductImages = function(leftIndex, middleIndex, rightIndex){
-  leftImage.src = ProductImage.allImages[leftIndex].url;
-  middleImage.src = ProductImage.allImages[middleIndex].url;
-  rightImage.src = ProductImage.allImages[rightIndex].url;
+  new ProductImage('bag', './img/bag.jpg');
+  new ProductImage('banana', './img/banana.jpg');
+  new ProductImage('bathroom', './img/bathroom.jpg');
+  new ProductImage('boots', './img/boots.jpg');
+  new ProductImage('breakfast', './img/breakfast.jpg');
+  new ProductImage('bubblegum', './img/bubblegum.jpg');
+  new ProductImage('chair', './img/chair.jpg');
+  new ProductImage('cthulhu', './img/cthulhu.jpg');
+  new ProductImage('dog-duck', './img/dog-duck.jpg');
+  new ProductImage('dragon', './img/dragon.jpg');
+  new ProductImage('pen', './img/pen.jpg');
+  new ProductImage('pet-sweep', './img/pet-sweep.jpg');
+  new ProductImage('scissors', './img/scissors.jpg');
+  new ProductImage('shark', './img/shark.jpg');
+  new ProductImage('sweep', './img/sweep.png');
+  new ProductImage('tauntaun', './img/tauntaun.jpg');
+  new ProductImage('unicorn', './img/unicorn.jpg');
+  new ProductImage('usb', './img/usb.gif');
+  new ProductImage('water-can', './img/water-can.jpg');
+  new ProductImage('wine-glass', './img/wine-glass.jpg');
 };
 
-
-// nor images that we displayed immediately before.
-
-
-var displayProductImages = function(){
-  var leftIndex = Math.round(Math.random() * ProductImage.allImages.length);
-
-  do {
-    var middleIndex = Math.round(Math.random() * ProductImage.allImages.length);
-  } while (middleIndex === leftIndex);
-
-  do {
-    var rightIndex = Math.round(Math.random() * ProductImage.allImages.length);
-  } while (rightIndex === middleIndex || rightIndex === leftIndex);
-
-  leftProductOnPage = ProductImage.allImages[leftIndex];
-  middleProductOnPage = ProductImage.allImages[middleIndex];
-  rightProductOnPage = ProductImage.allImages[rightIndex];
-
-  renderProductImages(leftIndex, middleIndex, rightIndex);
+var pickNonRepeating = function(currentPicks){
+  var index, product;
+  do{
+    index = getRandomIntInclusive(0, ProductImage.allImages.length - 1);
+    product = ProductImage.allImages[index];
+  } while (ProductImage.previousImages.includes(product) || currentPicks.includes(product));
+  return product;
 };
 
+var renderProductImages = function(){
+  var currentPicks = [];
+  var leftProduct = pickNonRepeating(currentPicks);
+  currentPicks.push(leftProduct);
+  var middleProduct = pickNonRepeating(currentPicks);
+  currentPicks.push(middleProduct);
+  var rightProduct = pickNonRepeating(currentPicks);
+  currentPicks.push(rightProduct);
 
-
+  leftImage.src = leftProduct.url;
+  middleImage.src = middleProduct.url;
+  rightImage.src = rightProduct.url;
+  ProductImage.previousImages = currentPicks;
+};
 
 var handleClicks = function(e){
-  if(totalClicks < 25){
-    var clickedProduct = e.target;
-    var id = clickedProduct.id;
-    if(id === 'left_image' || id === 'middle_image' || id === ' right_image'){
-      if(id === 'left_image'){
-        leftProductOnPage.clicks++;
-      }
-      if(id === 'middle_image'){
-        middleProductOnPage.clicks++;
-      }
-      if(id === 'right_image'){
-        rightProductOnPage.clicks++;
-      }
-      leftProductOnPage.timesShown++;
-      middleProductOnPage.timesShown++;
-      rightProductOnPage.timesShown++;
-    }
-  }
-  console.log('left', leftProductOnPage.name);
-  console.log('middle', middleProductOnPage.name);
-  console.log('right', rightProductOnPage.name);
+  clickCount++;
+  if(e.target.id === 'left_image'){
+    ProductImage.previousImages[0].clicks++;
 
-  totalClicks++;
-  if(totalClicks === 25){
+  }
+  if(e.target.id === 'middle_image'){
+    ProductImage.previousImages[1].clicks++;
+  }
+  if(e.target.id === 'right_image'){
+    ProductImage.previousImages[2].clicks++;
+  }
+  for (var i = 0; i < ProductImage.previousImages.length; i++){
+    ProductImage.previousImages[i].timesShown++;
+  }
+  if(clickCount < maxClicks){
+    renderProductImages();
+  } else {
     imageSection.removeEventListener('click', handleClicks);
   }
-  displayProductImages();
+  for(i = 0; i < ProductImage.allImages.length; i++){
+    var liEl = document.getElementById(ProductImage.allImages[i].name);
+    liEl.textContent = ProductImage.allImages[i].clicks + ' for the ' + ProductImage.allImages[i].name;
+  }
 };
 
-imageSection.addEventListener('click', handleClicks);
 
+var initPage = function(){
+  buildProducts();
+  renderProductImages();
 
-displayProductImages();
+  imageSection.addEventListener('click', handleClicks);
+};
+
+initPage();
