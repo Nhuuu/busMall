10b3f670ac7581ff898e3888fbd3540c4d0c3dc9
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-18 15:10:53 -0700

    chart is up

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var handleClicks = function(e){
   if(clickCount < maxClicks){
     renderProductImages();
   } else {
-    imageSection.removeEventListener('click', handleClicks);
+    makeBusChart();
   }
   for(i = 0; i < ProductImage.allImages.length; i++){
     var liEl = document.getElementById(ProductImage.allImages[i].name);
@@ -104,3 +104,57 @@ var initPage = function(){
 };
 
 initPage();
+
+function makeBusChart(){
+  var busChartCanvas = document.getElementById('busResults');
+
+  var percents = [];
+  var productNames = [];
+
+  for(var i = 0; i < ProductImage.allImages.length; i++){
+    var p = Math.floor((ProductImage.allImages[i].clicks / ProductImage.allImages[i].timesShown) * 100);
+    productNames.push(ProductImage.allImages[i].name);
+    percents.push(p);
+  }
+
+  var chartData = {
+    labels: productNames,
+    datasets: [{
+      label: '# of Votes',
+      data: percents,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  var busChartObject = {
+    type: 'bar',
+    data : chartData,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+
+  new Chart(busChartCanvas, busChartObject);
+}
