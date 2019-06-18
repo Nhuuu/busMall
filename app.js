8806ca8454c44d645fe0485126e45407b9bef878
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-17 17:31:03 -0700

    added image objects

--- app.js
+++ app.js
@@ -4,22 +4,66 @@
 Select three random photos from the image directory and display them side-by-side-by-side.
 
 Receive clicks on those displayed images
-
-Track those clicks for each image. 
-
-Track how many times each image is displayed, for statistical purposes.
+- Track those clicks for each image. 
+- Track how many times each image is displayed, for statistical purposes.
 
 Upon receiving a click, three new non-duplicating random images need to be displayed. 
-
   - no duplicates
   - nor images that we displayed immediately before.
 
-Constructor function that creates an object associated with each image, and has (at a minimum) properties for the name of the image (to be used for display purposes), its filepath, the number of times it has been shown, and the number of times it has been clicked. 
-
-You'll probably find it useful to create a property that contains a text string you can use as an ID in HTML.
+**You'll probably find it useful to create a property that contains a text string you can use as an ID in HTML.
 
 After 25 selections have been made, turn off the event listeners 
 
 Display a list of the products with votes received with each list item looking like "3 votes for the Banana Slicer".
 
 */
+
+// Globals
+var imageSection = document.getElementById('images');
+var leftImage = document.getElementById('left_image');
+var middleImage = document.getElementById('middle_image');
+var rightImage = document.getElementById('right_image');
+
+// can't click after 25
+var totalClicks = 25;
+
+var ProductImage = function(name, imgSrc, id){
+  this.name = name;
+  this.url = imgSrc;
+  this.timesShown = 0;
+  this.clicks = 0;
+  // this.id = id;
+
+  ProductImage.allImages.push(this);
+}
+
+ProductImage.allImages = [];
+
+// Instantiate all image objects
+
+new ProductImage('bag', './img/bag.jpg');
+new ProductImage('banana', './img/banana.jpg');
+new ProductImage('bathroom', './img/bathroom.jpg');
+new ProductImage('boots', './img/boots.jpg');
+new ProductImage('breakfast', './img/breakfast.jpg');
+new ProductImage('bubblegum', './img/bubblegum.jpg');
+new ProductImage('chair', './img/chair.jpg');
+new ProductImage('cthulu', './img/cthulu.jpg');
+new ProductImage('dog-duck', './img/dog-duck.jpg');
+new ProductImage('dragon', './img/dragon.jpg');
+new ProductImage('pen', './img/pen.jpg');
+new ProductImage('pet-sweep', './img/pet-sweep.jpg');
+new ProductImage('scissors', './img/scissors.jpg');
+new ProductImage('shark', './img/shark.jpg');
+new ProductImage('sweep', './img/sweep.png');
+new ProductImage('tauntaun', './img/tauntaun.jpg');
+new ProductImage('unicorn', './img/unicorn.jpg');
+new ProductImage('usb', './img/usb.gif');
+new ProductImage('water-can', './img/water-can.jpg');
+new ProductImage('wine-glass', './img/wine-glass.jpg');
+
+
+var renderProductImages = function (leftIndex, middleIndex, rightIndex){
+  leftImage.src = 
+}
