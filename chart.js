226b2c9d0d30e71fedcb4430a82c26129fc2db88
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-19 14:56:37 -0700

    moved chart to js file

---
+++ chart.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var busChartCanvas = document.getElementById('busResults');
+
+function makeBusChart(){
+  for(var i = 0; i < ProductImage.allImages.length; i++){
+    var p = Math.floor((ProductImage.allImages[i].clicks / ProductImage.allImages[i].timesShown) * 100);
+    productNames.push(ProductImage.allImages[i].name);
+    percents.push(p);
+  }
+
+  var chartData = {
+    labels: productNames,
+    datasets: [{
+      label: '% of Clicks per Times Shown',
+      data: percents,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1.2,
+    }]
+  };
+
+  var busChartObject = {
+    type: 'bar',
+    data : chartData,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+  new Chart(busChartCanvas, busChartObject);
+}
+
+
---
+++ localStorage.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+  Save totalClicks
+  Save each items clicks
+  Save percentages
+  Save all objects
+*/
+
+
+// var saveClickCount = clickCount;
+// var saveMaxClicks = maxClicks;
+// var savePercentages = percents;
+// var saveProducts = JSON.stringify(ProductImage.allImages);
+
+
+
+// localStorage.setItem('saveProducts', saveProducts);
+// localStorage.setItem('saveClickCount', saveClickCount);
+// localStorage.setItem('saveMaxClicks', saveMaxClicks);
+// localStorage.setItem('savePercentages', savePercentages);
+
+// var allSavedProducts = JSON.parse(localStorage.getItem('saveProducts'));
+
+// use JSON parsed objects in the chart.
