c7747e0b548e60caf8c1d6e066bcb7a28f02e96b
Nhu Trinh <nqtrinh87@gmail.com>
2019-06-20 08:18:53 -0700

    cleanup

--- localStorage.js
+++ localStorage.js
@@ -2,20 +2,3 @@
 
 var savedPercents = JSON.parse(localStorage.getItem('savedClicks'));
 
-
-
-
-// var saveClickCount = JSON.stringify(clickCount);
-// var savePercentages = JSON.stringify(percents);
-// var saveProductNames = JSON.stringify(productNames);
-
-
-
-// localStorage.setItem('saveClickCount', saveClickCount);
-// localStorage.setItem('savePercentages', savePercentages);
-// localStorage.setItem('saveProducts', saveProductNames);
-
-// var getProductNames = JSON.parse(localStorage.getItem('saveProductNames'));
-// var getClickCount = JSON.parse(localStorage.getItem('saveClickCount'));
-// var getPercentages = JSON.parse(localStorage.getItem('savePercentages'));
-
